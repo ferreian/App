094dc564426d4474937d040351d57c8c0f0179f4
ferreian <150438244+ferreian@users.noreply.github.com>
2024-04-05 15:43:13 -0400

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+
+# Título da página
+st.title("Bem Vindo!")
+
+# Descrição da aplicação
+st.write("""
+Esta aplicação permite analisar dados relacionados aos testes de faixa realizados pelo time DTC, fornecendo uma visão geral das métricas, estatísticas descritivas e visualizações interativas.
+""")
+
